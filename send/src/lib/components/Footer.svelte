544cdb3dc8ecfdd3a87e2a3585c63146b9133ef2
Gold Edem Hogan <edge37@outlook.com>
2025-06-09 17:40:52 +0100

    .

--- send/src/lib/components/Footer.svelte
+++
@@ -1,7 +0,0 @@
-<footer class="neumorphic p-4 mt-8">
-  <div class="flex justify-center gap-8">
-    <a href="/" class="text-secondary hover:text-primary transition-all">Services</a>
-    <a href="/contact" class="text-secondary hover:text-primary transition-all">Contact</a>
-    <a href="/more" class="text-secondary hover:text-primary transition-all">More</a>
-  </div>
-</footer> 
